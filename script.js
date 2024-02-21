b701e4428e534686077808bfa948522837fc0117
M. Azmi Al Fadillah <120736981+Azmi190104@users.noreply.github.com>
2024-02-21 18:35:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const btn1 = document.querySelector('.email form .button')
+const arr = ['azmi']
+
+
+
+function validation(){
+    let inpt = document.querySelector('.email form input[type="text"]').value
+    let text = document.querySelector('.text p:nth-child(2)')
+    let form = document.querySelector('.email form input[type="text"]')
+    let overlay = document.querySelector('.overlay')
+
+    // inpt.style.backgroundColor='salmon'
+
+    if(inpt === ''){
+        text.style.display='block'
+        form.classList.add('unvalid')
+        form.style.transition='.3s'
+        
+    }
+    if(inpt == arr){
+        // window.open("winchat-main/index.html")
+        overlay.style.display='block'
+    }
+
+}
+
+
+
+
+
+
+
+
